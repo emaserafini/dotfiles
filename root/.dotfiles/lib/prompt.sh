5a9d6a3025bcd489ff3e7a160a09e677c4704d38
Emanuele Serafini <ema.serafini@gmail.com>
2014-01-14 16:57:00 +0100

    Color prompt improve

--- root/.dotfiles/lib/prompt.sh
+++ root/.dotfiles/lib/prompt.sh
@@ -1,9 +1,30 @@
 ### PROMPT ###
 
+# ANSI color codes
+RS="\[\033[0m\]"    # reset
+HC="\[\033[1m\]"    # hicolor
+UL="\[\033[4m\]"    # underline
+INV="\[\033[7m\]"   # inverse background and foreground
+FBLK="\[\033[30m\]" # foreground black
+FRED="\[\033[31m\]" # foreground red
+FGRN="\[\033[32m\]" # foreground green
+FYEL="\[\033[33m\]" # foreground yellow
+FBLE="\[\033[34m\]" # foreground blue
+FMAG="\[\033[35m\]" # foreground magenta
+FCYN="\[\033[36m\]" # foreground cyan
+FWHT="\[\033[37m\]" # foreground white
+
+BBLK="\[\033[40m\]" # background black
+BRED="\[\033[41m\]" # background red
+BGRN="\[\033[42m\]" # background green
+BYEL="\[\033[43m\]" # background yellow
+BBLE="\[\033[44m\]" # background blue
+BMAG="\[\033[45m\]" # background magenta
+BCYN="\[\033[46m\]" # background cyan
+BWHT="\[\033[47m\]" # background white
+
 GIT_PS1_SHOWDIRTYSTATE=1 #... untagged(*) and staged(+) changes
 GIT_PS1_SHOWSTASHSTATE=1 #... if something is stashed($)
 GIT_PS1_SHOWUNTRACKEDFILES=1 #... untracked files(%)
 
-# PS1="\W\$(__git_ps1) \$ "
-PS1='\[\033[0;35m\]$(rvm-prompt u)\[\033[0m\] \[\033[0;32m\]\h:\u \[\033[0;34m\]\W\[\033[0m\]\[\033[0;35m\]$(__git_ps1 " (%s)")\[\033[0m\]\$ '
-# PS1='\[\033[0;35m\]$(rvm-prompt u)\[\033[0m\] \[\033[0;32m\]\h:\u \[\033[0;34m\]\W\[\033[0m\]\$ '
+PS1="$FCYN\$(rvm-prompt u) $HC$FYEL\h$RS:$HC$FYEL\u$RS $FGRN\W$FMAG\$(__git_ps1 ' (%s)') $RS\$ "
